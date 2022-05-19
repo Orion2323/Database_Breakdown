dea6685322abdf97de6f7650a1f301df3187d262
Giancarlos Dominguez <Giancarlosd@smu.edu>
2022-05-19 15:37:31 -0500

    Updated file to reflect changes in branch

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// Import route handlers
-const studentRoutes = require('./routes/student');
-const courseRoutes = require('./routes/course');
+// Import routes
+const userRoutes = require('./routes/users')
+const sessionRoutes = require('./routes/session');
 
 // Import middleware
-const {createModelMiddleware, disconnectFromDatabaseMiddleware} = require('./middleware/model-middleware.js');
+const {createModelMiddleware} = require('./middleware/model-middleware.js');
+const {authenticateJWT} = require('./middleware/auth.js');
 
 // Define express app instance
 const app = express();
 const port = 3000;
-app.use(bodyParser.json());
 
 app.use(createModelMiddleware);
+app.use(bodyParser.json());
 
 // health route to verify that connection to database is active
 app.get('/health', (req, res, next) => {
@@ -26,10 +27,9 @@ app.get('/health', (req, res, next) => {
     next();
 });
 
-app.use('/student', studentRoutes);
-app.use('/course', courseRoutes);
-
-app.use(disconnectFromDatabaseMiddleware);
+// define route handlers here
+app.use('/session', sessionRoutes);
+app.use('/user', userRoutes, authenticateJWT);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
