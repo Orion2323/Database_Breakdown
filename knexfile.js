73c53d95986e95371b7c8f4775b4fcbf3cd91a8e
Giancarlos Dominguez <Giancarlosd@smu.edu>
2022-05-19 15:33:14 -0500

    Implemented knex into program

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+module.exports = {
+    development: {
+        client: 'mysql',
+        debug: true,
+
+        connection: {
+            host: '127.0.0.1',
+            port: 3306,
+            user: 'root',
+            password: 'password',
+            insecureAuth: true,
+            database: 'newDB'
+        }
+    }
+};
