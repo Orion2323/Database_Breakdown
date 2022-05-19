06999cf17221a2b063faefd2ce47f08cbead5125
Giancarlos Dominguez <Giancarlosd@smu.edu>
2022-05-19 15:36:36 -0500

    Deleted database connection method

--- middleware/model-middleware.js
+++ middleware/model-middleware.js
@@ -1,28 +1,13 @@
-const connectToDatabase = require('../models/connect-to-database.js');
-const Student = require('../models/student.js');
-const Course = require('../models/course.js');
+const User = require('../models/users');
 
 const createModelMiddleware = async (req, res, next) => {
-    console.log('Connecting to database');
-    const {DBQuery, disconnect} = await connectToDatabase();
-
     req.models = {
-        student: new Student(DBQuery, disconnect),
-        course: new Course(DBQuery, disconnect)
+        user: User
     }
 
-    req.disconnect = disconnect;
-    next();
-}
-
-const disconnectFromDatabaseMiddleware = (req, res, next) => {
-    console.log('Disconnecting from database\n');
-    
-    req.disconnect();
     next();
 }
 
 module.exports = {
     createModelMiddleware,
-    disconnectFromDatabaseMiddleware
 }
