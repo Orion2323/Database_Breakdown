e784d4c67c862377fb607acd944d0b2c7404b20c
Giancarlos Dominguez <Giancarlosd@smu.edu>
2022-05-24 21:29:59 -0500

    Updated routes to call controllers instead of models

--- routes/session.js
+++ routes/session.js
@@ -1,18 +1,19 @@
-const sessionModel = require('../models/session.js');
+const sessionController = require('../controllers/session.js');
+const userController = require('../controllers/users.js');
+
 const express = require('express');
 const router = express.Router();
 
-// POST route to create new user account
+// POST route to verify is user credentials are valid
 router.get('/', async (req, res, next) => {
     try {
-        const result = await req.models.user.authenticateUser(req.body.email,req.body.password);
+        const result = await sessionController.authenticateUser(req.body.email,req.body.password);
 
-        // check if result was successful or not
+        // check for errors
         if (result.error != undefined) {
             res.status(result.status).json(result.error);
         } else {
-            const token = await sessionModel.createAuthToken(req.body.email,"User");
-            res.status(200).json({Token: token});
+            res.status(200).json({Token: result});
         }
     } catch (err) {
         res.status(500).json({message: err.toString()});
@@ -21,10 +22,10 @@ router.get('/', async (req, res, next) => {
     next();
 });
 
-// POST route to verify is user credentials are valid
+// POST route to create new user account
 router.post('/', async (req, res, next) => {
     try {
-        const result = await req.models.user.createNewUser(req.body.name,req.body.email,req.body.password);
+        const result = await userController.createNewUser(req.body.name,req.body.email,req.body.password);
 
         // check if result was successful or not
         if (result.error != undefined) {
